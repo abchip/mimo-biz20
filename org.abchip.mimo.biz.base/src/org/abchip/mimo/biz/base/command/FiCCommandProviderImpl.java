96dab310b2f764f0fc1655fc0885fa5d4de3a9c5
dev@abchip.it <dev@abchip.it>
2020-03-06 18:59:48 +0100

    Implementation FiC Importation command

---
+++ org.abchip.mimo.biz.base/src/org/abchip/mimo/biz/base/command/FiCCommandProviderImpl.java
@@ -0,0 +1,289 @@
+/**
+ *  Copyright (c) 2017, 2020 ABChip and others.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ */
+package org.abchip.mimo.biz.base.command;
+
+import java.io.UnsupportedEncodingException;
+import java.math.BigDecimal;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.inject.Inject;
+
+import org.abchip.mimo.application.Application;
+import org.abchip.mimo.context.Context;
+import org.abchip.mimo.core.base.BaseCommandProviderImpl;
+import org.abchip.mimo.entity.EntityContainer;
+import org.abchip.mimo.entity.EntityFactory;
+import org.abchip.mimo.entity.EntityIdentifiable;
+import org.abchip.mimo.resource.ResourceManager;
+import org.abchip.mimo.resource.ResourceWriter;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.eclipse.osgi.framework.console.CommandInterpreter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class FiCCommandProviderImpl extends BaseCommandProviderImpl {
+	private static final String urlFiC = "https://api.fattureincloud.it:443/v1/";
+	private static final String body = "{\"api_uid\": \"673532\", \"api_key\": \"38319cbc95dddf6ff7d744491bd823e7\", \"pagina\": ?}";
+
+	@Inject
+	private ResourceManager resourceManager;
+	@Inject
+	private Application application;
+
+	
+	public <E extends EntityIdentifiable> void _importFicAll(CommandInterpreter interpreter) throws Exception {
+		importFicRegistry();
+		importFicProduct();
+	}
+
+	public <E extends EntityIdentifiable> void _importFicRegistry(CommandInterpreter interpreter) throws Exception {
+		importFicRegistry();
+	}
+
+	public <E extends EntityIdentifiable> void _importFicProduct(CommandInterpreter interpreter) throws Exception {
+		importFicProduct();
+	}
+	
+	@SuppressWarnings("resource")
+	private void importFicRegistry() throws Exception {
+//		Context context = this.getContext();
+		Context context = application.getContext();
+		AtomicInteger partyCounter = new AtomicInteger(9999);
+		AtomicInteger contactMechCounter = new AtomicInteger(9999);
+
+		importRegistry(context, partyCounter, contactMechCounter, "FiC_Customer", "clienti", "lista_clienti", "CUSTOMER");
+		importRegistry(context, partyCounter, contactMechCounter, "FiC_Supplier", "fornitori", "lista_fornitori", "SUPPLIER");
+	}
+	
+	@SuppressWarnings("unchecked")
+	private void importRegistry(Context context, AtomicInteger partyCounter, AtomicInteger contactMechCounter, String containerName, String typeSubject, String nameArray, String role) throws UnsupportedEncodingException {
+		EntityContainer container = EntityFactory.eINSTANCE.createEntityContainer();
+		container.setName(containerName);
+
+		ArrayList<LinkedHashMap<String, Object>> arrayList = null;
+		int current_page = 0;
+		int number_page = 0;
+
+		// ogni chiamata restituisce al massimo 500 elementi, si può paginare
+		for (int i = 0; i < 5; i++) {
+
+			// Semplice impostazione del json di body
+			StringEntity params = new StringEntity(body.replace("?", Integer.toString(i + 1)));
+
+			HttpPost postMethod = null;
+			HashMap<?, ?> fiCResult = null;
+			try (CloseableHttpClient client = HttpClients.custom().build()) {
+				URI uri = new URIBuilder().setPath(urlFiC + typeSubject+"/lista").build();
+				postMethod = new HttpPost(uri);
+				postMethod.setEntity(params);
+				try (CloseableHttpResponse postResponse = client.execute(postMethod)) {
+					String responseString = new BasicResponseHandler().handleResponse(postResponse);
+					System.out.println(responseString);
+					if (postResponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+		//				String responseString = "{\"lista_clienti\":[{\"id\":\"33363131\",\"nome\":\"Giuliano Giancristofaro\",\"referente\":\"\",\"indirizzo_via\":\"via rezzara 1\",\"indirizzo_cap\":\"24036\",\"indirizzo_citta\":\"Ponte San Pietro\",\"indirizzo_provincia\":\"BG\",\"indirizzo_extra\":\"\",\"paese\":\"Italia\",\"mail\":\"giuliano.giancristofaro@gmail.com\",\"pec\":\"giuliano.giancristofaro@legalmail.it\",\"tel\":\"001 0123456\",\"fax\":\"123 5684798\",\"piva\":\"02580920169\",\"cf\":\"GNCGLN73P01H501V\",\"termini_pagamento\":\"30\",\"pagamento_fine_mese\":false,\"val_iva_default\":\"22\",\"desc_iva_default\":\"\",\"extra\":\"\",\"PA\":true,\"PA_codice\":\"\"}],\"pagina_corrente\":1,\"numero_pagine\":1,\"success\":true}";
+						fiCResult = new ObjectMapper().readValue(responseString, HashMap.class);
+						boolean success = (boolean) fiCResult.get("success");
+						if (success) {
+							arrayList = (ArrayList<LinkedHashMap<String, Object>>) fiCResult.get(nameArray);
+							current_page = (int) fiCResult.get("pagina_corrente");
+							number_page = (int) fiCResult.get("numero_pagine");
+							//
+							for (LinkedHashMap<String, Object> record : arrayList) {
+							// TODO non viene riportata la tipologia del cliente. 
+							// TODO Al momento tratto tutti come Party_Group
+								String partyId = Integer.toString(partyCounter.incrementAndGet());
+								String id = (String) record.get("id");
+								String nome = (String) record.get("nome");
+								String referente = (String) record.get("referente");
+								
+								FiCCommandUtils.createPartyGroup(container, context, partyId, id, nome, referente);
+								FiCCommandUtils.createPartyRole(container, context, partyId, role);
+								
+								// Indirizzo 
+								String indirizzo_via = (String) record.get("indirizzo_via");
+								String indirizzo_citta = (String) record.get("indirizzo_citta");
+								String indirizzo_cap = (String) record.get("indirizzo_cap");
+								String indirizzo_provincia = (String) record.get("indirizzo_provincia");
+								String indirizzo_paese = (String) record.get("paese");
+								if (!(indirizzo_via).isEmpty()
+									|| !(indirizzo_citta).isEmpty()
+									|| !(indirizzo_cap).isEmpty()
+									|| !(indirizzo_provincia).isEmpty()) {
+									FiCCommandUtils.createPostaAddress(container, context, partyId, contactMechCounter, nome, indirizzo_via, indirizzo_citta, indirizzo_cap, indirizzo_provincia, indirizzo_paese);
+								}
+								// mail
+								if (record.get("mail")!= null && !((String) record.get("mail")).isEmpty()) {
+									FiCCommandUtils.createContactMechMail(container, context, (String) record.get("mail"), partyId, contactMechCounter,"EMAIL_ADDRESS", "PRIMARY_EMAIL");
+								}
+								// pec
+								if (record.get("pec")!= null && !((String) record.get("pec")).isEmpty()) {
+									FiCCommandUtils.createContactMechMail(container, context, (String) record.get("mail"), partyId, contactMechCounter,"EMAIL_ADDRESS", "BILLING_EMAIL");
+								}
+								
+								// tel
+								if (record.get("tel")!= null && !((String) record.get("tel")).isEmpty()) {
+									FiCCommandUtils.createContactMechTelecom(container, context, (String) record.get("tel"), partyId, contactMechCounter, "TELECOM_NUMBER", "PRIMARY_PHONE");
+								}
+								// fax
+								if (record.get("fax")!= null && !((String) record.get("fax")).isEmpty()) {
+									FiCCommandUtils.createContactMechTelecom(container, context, (String) record.get("tel"), partyId, contactMechCounter, "TELECOM_NUMBER", "FAX_NUMBER");
+								}
+								// piva
+								if (record.get("piva")!= null && !((String) record.get("piva")).isEmpty()) {
+									FiCCommandUtils.createTaxAuth(container, context, partyId, (String) record.get("piva"));
+								}
+								// cf
+								if (record.get("cf")!= null && !((String) record.get("cf")).isEmpty()) {
+									FiCCommandUtils.createIdentificationCard(container, context, partyId, (String) record.get("cf"));
+								}
+								// termini_pagamento
+								if (record.get("termini_pagamento")!= null && !((String) record.get("termini_pagamento")).isEmpty()) {}
+								// pagamento_fine_mese
+								//						if (!((String) record.get("pagamento_fine_mese")).isEmpty()) {}
+								// val_iva_default
+								if (record.get("val_iva_default")!= null && !((String) record.get("val_iva_default")).isEmpty()) {}
+								// desc_iva_default
+								if (record.get("desc_iva_default")!= null && !((String) record.get("desc_iva_default")).isEmpty()) {}
+								// extra
+								if (record.get("extra")!= null && !((String) record.get("extra")).isEmpty()) {}
+								// PA
+								//						if (!((String) record.get("PA")).isEmpty()) {}
+								// PA_codice
+								if (record.get("PA_codice")!= null && !((String) record.get("PA_codice")).isEmpty()) {}
+							}
+						}
+					} else {
+						System.err.println("Error");
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			// Leave
+			if (number_page == current_page)
+				break;
+		}
+		// Save container
+		ResourceWriter<EntityIdentifiable> entityWriter = resourceManager.getResourceWriter(context, "EntityContainer");
+		entityWriter.create(container, true);
+	}
+
+	@SuppressWarnings({ "resource", "unchecked" })
+	private void importFicProduct() throws Exception {
+		/*
+		 * 
+		 * {
+			  "id": "14806135",
+			  "cod": "0001",
+			  "nome": "Prodotto 1",
+			  "prezzo_netto": "10.00000",
+			  "prezzo_ivato": false,
+			  "costo": "0.00000",
+			  "valore_iva": "-1",
+			  "desc_iva": "-1",
+			  "um": "NR",
+			  "categoria": "Prodotto finito",
+			  "desc": "Prodotto numero 1",
+			  "note": "",
+			  "magazzino": false
+    		}
+		 */
+//		Context context = this.getContext();
+		Context context = application.getContext();
+		
+		EntityContainer container = EntityFactory.eINSTANCE.createEntityContainer();
+		container.setName("FiC_Product");
+
+		ArrayList<LinkedHashMap<String, Object>> arrayList = null;
+		int current_page = 0;
+		int number_page = 0;
+
+		// ogni chiamata restituisce al massimo 500 elementi, si può paginare
+		for (int i = 0; i < 5; i++) {
+
+			// Semplice impostazione del json di body
+			StringEntity params = new StringEntity(body.replace("?", Integer.toString(i + 1)));
+
+			HttpPost postMethod = null;
+			HashMap<?, ?> fiCResult = null;
+			try (CloseableHttpClient client = HttpClients.custom().build()) {
+				URI uri = new URIBuilder().setPath(urlFiC + "prodotti/lista").build();
+				postMethod = new HttpPost(uri);
+				postMethod.setEntity(params);
+				try (CloseableHttpResponse postResponse = client.execute(postMethod)) {
+					String responseString = new BasicResponseHandler().handleResponse(postResponse);
+					System.out.println(responseString);
+					if (postResponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+		//				String responseString = "{\"lista_clienti\":[{\"id\":\"33363131\",\"nome\":\"Giuliano Giancristofaro\",\"referente\":\"\",\"indirizzo_via\":\"via rezzara 1\",\"indirizzo_cap\":\"24036\",\"indirizzo_citta\":\"Ponte San Pietro\",\"indirizzo_provincia\":\"BG\",\"indirizzo_extra\":\"\",\"paese\":\"Italia\",\"mail\":\"giuliano.giancristofaro@gmail.com\",\"pec\":\"giuliano.giancristofaro@legalmail.it\",\"tel\":\"001 0123456\",\"fax\":\"123 5684798\",\"piva\":\"02580920169\",\"cf\":\"GNCGLN73P01H501V\",\"termini_pagamento\":\"30\",\"pagamento_fine_mese\":false,\"val_iva_default\":\"22\",\"desc_iva_default\":\"\",\"extra\":\"\",\"PA\":true,\"PA_codice\":\"\"}],\"pagina_corrente\":1,\"numero_pagine\":1,\"success\":true}";
+						fiCResult = new ObjectMapper().readValue(responseString, HashMap.class);
+						boolean success = (boolean) fiCResult.get("success");
+						if (success) {
+							arrayList = (ArrayList<LinkedHashMap<String, Object>>) fiCResult.get("lista_prodotti");
+							current_page = Integer.parseInt((String) fiCResult.get("pagina_corrente")) ;
+							number_page = (int) fiCResult.get("numero_pagine");
+							//
+							for (LinkedHashMap<String, Object> record : arrayList) {
+								
+								String id = (String) record.get("id");
+								String cod = (String) record.get("cod");
+								String nome = (String) record.get("nome");
+								String desc = (String) record.get("desc");
+								String categoria = (String) record.get("categoria");
+								String um = (String) record.get("um");
+								FiCCommandUtils.createProduct(container, context, id, cod, nome, desc, categoria, um);
+
+								BigDecimal price =  null;
+								if((String) record.get("prezzo_netto") != null) {
+									price =  new BigDecimal((String) record.get("prezzo_netto"));
+								} else if ((String) record.get("prezzo_lordo") != null) {
+									price =  new BigDecimal((String) record.get("prezzo_lordo"));
+								} else {
+									price =  new BigDecimal(0);
+								}
+								
+								BigDecimal cost =  new BigDecimal((String) record.get("costo"));
+								boolean iva = (boolean) record.get("prezzo_ivato");
+								String ivaValue = (String) record.get("valore_iva");
+								String ivaDescription = (String) record.get("desc_iva");
+								FiCCommandUtils.createProductPrice(container, context, nome, price, cost, iva, ivaValue, ivaDescription);
+							}
+						}
+					} else {
+						System.err.println("Error");
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			// Leave
+			if (number_page == current_page)
+				break;
+		}
+		// Save container
+		ResourceWriter<EntityIdentifiable> entityWriter = resourceManager.getResourceWriter(context, "EntityContainer");
+		entityWriter.create(container, true);
+	}
+	
+	@Override
+	public String getHelp() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
---
+++ org.abchip.mimo.biz.base/src/org/abchip/mimo/biz/base/command/FiCCommandUtils.java
@@ -0,0 +1,231 @@
+/**
+ *  Copyright (c) 2017, 2020 ABChip and others.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ */
+package org.abchip.mimo.biz.base.command;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.abchip.mimo.biz.accounting.tax.PartyTaxAuthInfo;
+import org.abchip.mimo.biz.accounting.tax.TaxFactory;
+import org.abchip.mimo.biz.common.geo.Geo;
+import org.abchip.mimo.biz.common.status.StatusItem;
+import org.abchip.mimo.biz.common.uom.Uom;
+import org.abchip.mimo.biz.party.contact.ContactFactory;
+import org.abchip.mimo.biz.party.contact.ContactMech;
+import org.abchip.mimo.biz.party.contact.ContactMechPurposeType;
+import org.abchip.mimo.biz.party.contact.ContactMechType;
+import org.abchip.mimo.biz.party.contact.PartyContactMech;
+import org.abchip.mimo.biz.party.contact.PartyContactMechPurpose;
+import org.abchip.mimo.biz.party.contact.PostalAddress;
+import org.abchip.mimo.biz.party.contact.TelecomNumber;
+import org.abchip.mimo.biz.party.party.Party;
+import org.abchip.mimo.biz.party.party.PartyFactory;
+import org.abchip.mimo.biz.party.party.PartyGroup;
+import org.abchip.mimo.biz.party.party.PartyIdentification;
+import org.abchip.mimo.biz.party.party.PartyIdentificationType;
+import org.abchip.mimo.biz.party.party.PartyRole;
+import org.abchip.mimo.biz.party.party.PartyType;
+import org.abchip.mimo.biz.party.party.RoleType;
+import org.abchip.mimo.biz.product.price.PriceFactory;
+import org.abchip.mimo.biz.product.price.ProductPrice;
+import org.abchip.mimo.biz.product.price.ProductPricePurpose;
+import org.abchip.mimo.biz.product.price.ProductPriceType;
+import org.abchip.mimo.biz.product.product.Product;
+import org.abchip.mimo.biz.product.product.ProductFactory;
+import org.abchip.mimo.biz.product.store.ProductStoreGroup;
+import org.abchip.mimo.context.Context;
+import org.abchip.mimo.entity.EntityContainer;
+import org.abchip.mimo.resource.ResourceManager;
+
+public class FiCCommandUtils {
+
+	public static void createContactMechTelecom(EntityContainer container, Context context, String telephone, String partyId, AtomicInteger contactMechCounter, String contactMechType, String contactMechPurposeType) {
+		ResourceManager resourceManager = context.get(ResourceManager.class);
+		// TelecomNumber
+		TelecomNumber telecomNumber = ContactFactory.eINSTANCE.createTelecomNumber();
+		String contactMechId = Integer.toString(contactMechCounter.incrementAndGet());
+		telecomNumber.setContactMechId(contactMechId);
+		telecomNumber.setContactMechTypeId(resourceManager.getFrame(context, ContactMechType.class).createProxy(contactMechType));
+		telecomNumber.setContactNumber(telephone);
+		container.getContents().add(telecomNumber);
+		
+		// PartyContactMech
+		PartyContactMech partyContactMech = ContactFactory.eINSTANCE.createPartyContactMech();
+		partyContactMech.setPartyId(resourceManager.getFrame(context, Party.class).createProxy(partyId));
+		partyContactMech.setContactMechId(resourceManager.getFrame(context, ContactMech.class).createProxy(contactMechId));
+		partyContactMech.setFromDate(new Date());
+		container.getContents().add(partyContactMech);
+
+		// PartyContactMechPurpose
+		PartyContactMechPurpose partyContactMechPurpose = ContactFactory.eINSTANCE.createPartyContactMechPurpose();
+		partyContactMechPurpose.setPartyId(resourceManager.getFrame(context, Party.class).createProxy(partyId));
+		partyContactMechPurpose.setContactMechId(resourceManager.getFrame(context, ContactMech.class).createProxy(contactMechId));
+		partyContactMechPurpose.setContactMechPurposeTypeId(resourceManager.getFrame(context, ContactMechPurposeType.class).createProxy(contactMechPurposeType));
+		partyContactMechPurpose.setFromDate(new Date());
+		container.getContents().add(partyContactMechPurpose);
+	}
+
+	public static void createContactMechMail(EntityContainer container, Context context, String email, String partyId, AtomicInteger contactMechCounter, String contactMechType, String contactMechPurposeType) {
+		ResourceManager resourceManager = context.get(ResourceManager.class);
+
+		// ContactMech
+		ContactMech contactMech = ContactFactory.eINSTANCE.createContactMech();
+		String contactMechId = Integer.toString(contactMechCounter.incrementAndGet());
+		contactMech.setContactMechId(contactMechId);
+		contactMech.setInfoString(email);
+		contactMech.setContactMechTypeId(resourceManager.getFrame(context, ContactMechType.class).createProxy(contactMechType));
+		container.getContents().add(contactMech);
+
+		// PartyContactMech
+		PartyContactMech partyContactMech = ContactFactory.eINSTANCE.createPartyContactMech();
+		partyContactMech.setPartyId(resourceManager.getFrame(context, Party.class).createProxy(partyId));
+		partyContactMech.setContactMechId(resourceManager.getFrame(context, ContactMech.class).createProxy(contactMechId));
+		partyContactMech.setFromDate(new Date());
+		container.getContents().add(partyContactMech);
+
+		// PartyContactMechPurpose
+		PartyContactMechPurpose partyContactMechPurpose = ContactFactory.eINSTANCE.createPartyContactMechPurpose();
+		partyContactMechPurpose.setPartyId(resourceManager.getFrame(context, Party.class).createProxy(partyId));
+		partyContactMechPurpose.setContactMechId(resourceManager.getFrame(context, ContactMech.class).createProxy(contactMechId));
+		partyContactMechPurpose.setContactMechPurposeTypeId(resourceManager.getFrame(context, ContactMechPurposeType.class).createProxy(contactMechPurposeType));
+		partyContactMechPurpose.setFromDate(new Date());
+		container.getContents().add(partyContactMechPurpose);
+	}
+
+	public static void createTaxAuth(EntityContainer container, Context context, String partyId, String piva) {
+		ResourceManager resourceManager = context.get(ResourceManager.class);
+		// PartyTaxAuthInfo
+		PartyTaxAuthInfo partyTaxAuthInfo = TaxFactory.eINSTANCE.createPartyTaxAuthInfo();
+		partyTaxAuthInfo.setPartyId(resourceManager.getFrame(context, Party.class).createProxy(partyId));
+		partyTaxAuthInfo.setFromDate(new Date());
+		partyTaxAuthInfo.setTaxAuthGeoId("ITA");
+		partyTaxAuthInfo.setTaxAuthPartyId("ITA_ADE");
+		partyTaxAuthInfo.setPartyTaxId("IT-" + piva);
+		container.getContents().add(partyTaxAuthInfo);
+	}
+
+	public static void createIdentificationCard(EntityContainer container, Context context, String partyId, String cf) {
+		ResourceManager resourceManager = context.get(ResourceManager.class);
+		// PartyTaxAuthInfo
+		PartyIdentification partyIdentification = PartyFactory.eINSTANCE.createPartyIdentification();
+		partyIdentification.setPartyId(resourceManager.getFrame(context, Party.class).createProxy(partyId));
+		partyIdentification.setPartyIdentificationTypeId(resourceManager.getFrame(context, PartyIdentificationType.class).createProxy("CARD_ID"));
+		partyIdentification.setIdValue(cf);
+		container.getContents().add(partyIdentification);
+	}
+
+	public static void createPartyGroup(EntityContainer container, Context context, String partyId, String id, String nome, String referente) {
+		ResourceManager resourceManager = context.get(ResourceManager.class);
+		PartyGroup partyGroup = PartyFactory.eINSTANCE.createPartyGroup();
+		partyGroup.setPartyId(partyId);
+		partyGroup.setStatusId(resourceManager.getFrame(context, StatusItem.class).createProxy("PARTY_ENABLED"));
+		partyGroup.setPartyTypeId(resourceManager.getFrame(context, PartyType.class).createProxy("PARTY_GROUP"));
+		partyGroup.setPreferredCurrencyUomId(resourceManager.getFrame(context, Uom.class).createProxy("EUR"));
+		// id
+		partyGroup.setExternalId(id);
+		// nome
+		partyGroup.setGroupName(nome);
+		// referente
+		// TODO al momento non so dove scrivere l'informazione "referente"
+		if (!referente.isEmpty()) {
+			
+		}
+		container.getContents().add(partyGroup);
+	}
+
+	public static void createPartyRole(EntityContainer container, Context context, String partyId, String string) {
+		ResourceManager resourceManager = context.get(ResourceManager.class);
+		// PartyRole
+		PartyRole partyRole = PartyFactory.eINSTANCE.createPartyRole();
+		partyRole.setPartyId(resourceManager.getFrame(context, Party.class).createProxy(partyId));
+		partyRole.setRoleTypeId(resourceManager.getFrame(context, RoleType.class).createProxy("CUSTOMER"));
+		container.getContents().add(partyRole);
+	}
+
+	public static void createPostaAddress(EntityContainer container, Context context, String partyId, AtomicInteger contactMechCounter, 
+			String nome, String indirizzo_via, String indirizzo_citta, String indirizzo_cap, String indirizzo_provincia, String indirizzo_paese) {
+		ResourceManager resourceManager = context.get(ResourceManager.class);
+		// PostaAddress
+		PostalAddress postalAddress = ContactFactory.eINSTANCE.createPostalAddress();
+		String contactMechId = Integer.toString(contactMechCounter.incrementAndGet());
+		postalAddress.setContactMechId(contactMechId);
+		postalAddress.setToName(nome);
+		// indirizzo_via
+		postalAddress.setAddress1(indirizzo_via);
+		// indirizzo_città
+		postalAddress.setCity(indirizzo_citta);
+		// indirizzo_cap
+		postalAddress.setPostalCode(indirizzo_cap);
+		postalAddress.setContactMechTypeId(resourceManager.getFrame(context, ContactMechType.class).createProxy("POSTAL_ADDRESS"));
+		// paese
+		if(indirizzo_paese.equals("Italia"))
+			postalAddress.setCountryGeoId(resourceManager.getFrame(context, Geo.class).createProxy("ITA"));
+		// indirizzo_provincia
+		postalAddress.setStateProvinceGeoId(resourceManager.getFrame(context, Geo.class).createProxy(indirizzo_provincia));
+		// indirizzo_extra
+		// TODO non so
+		container.getContents().add(postalAddress);
+
+		// PartyContactMech
+		PartyContactMech partyContactMech = ContactFactory.eINSTANCE.createPartyContactMech();
+		partyContactMech.setPartyId(resourceManager.getFrame(context, Party.class).createProxy(partyId));
+		partyContactMech.setContactMechId(resourceManager.getFrame(context, ContactMech.class).createProxy(contactMechId));
+		partyContactMech.setFromDate(new Date());
+		container.getContents().add(partyContactMech);
+
+		// PartyContactMechPurpose
+		PartyContactMechPurpose partyContactMechPurpose = ContactFactory.eINSTANCE.createPartyContactMechPurpose();
+		partyContactMechPurpose.setPartyId(resourceManager.getFrame(context, Party.class).createProxy(partyId));
+		partyContactMechPurpose.setContactMechId(resourceManager.getFrame(context, ContactMech.class).createProxy(contactMechId));
+		partyContactMechPurpose.setContactMechPurposeTypeId(resourceManager.getFrame(context, ContactMechPurposeType.class).createProxy("GENERAL_LOCATION"));
+		partyContactMechPurpose.setFromDate(new Date());
+		container.getContents().add(partyContactMechPurpose);
+	}
+
+	public static void createProduct(EntityContainer container, Context context, String id, String cod, String nome, String desc, String categoria, String um) {
+		// Product
+		Product product = ProductFactory.eINSTANCE.createProduct();
+		product.setProductId(nome);
+		product.setProductName(desc);
+		product.setInternalName(cod);
+		product.setComments(id);
+		if(!categoria.isEmpty()) {
+			// TODO come la trattiamo? è un campo del tutto libero
+		}
+		if(!um.isEmpty()) {
+			// TODO come la trattiamo? è un campo del tutto libero
+		}
+		container.getContents().add(product);
+	}
+
+	public static void createProductPrice(EntityContainer container, Context context, String nome, BigDecimal price,
+			BigDecimal cost, boolean iva, String ivaValue, String ivaDescription) {
+		ResourceManager resourceManager = context.get(ResourceManager.class);
+		// ProductPrice
+		ProductPrice productPrice = PriceFactory.eINSTANCE.createProductPrice();
+		productPrice.setProductId(resourceManager.getFrame(context, Product.class).createProxy(nome));
+		productPrice.setFromDate(new Date());
+		productPrice.setPrice(price);
+		productPrice.setProductPriceTypeId(resourceManager.getFrame(context, ProductPriceType.class).createProxy("DEFAULT_PRICE"));
+		productPrice.setProductPricePurposeId(resourceManager.getFrame(context, ProductPricePurpose.class).createProxy("PURCHASE"));
+		productPrice.setCurrencyUomId(resourceManager.getFrame(context, Uom.class).createProxy("EUR"));
+		productPrice.setProductStoreGroupId(resourceManager.getFrame(context, ProductStoreGroup.class).createProxy("_NA_"));
+		productPrice.setTaxInPrice(iva);
+		if(!ivaValue.equals("-1")) {
+			productPrice.setTaxAmount(new BigDecimal(ivaValue));
+		}
+		container.getContents().add(productPrice);
+	
+		if(cost.compareTo(BigDecimal.ZERO) > 0) {
+			// TODO scrivere un nuovo ProductPrice
+		}
+	}
+
+}
