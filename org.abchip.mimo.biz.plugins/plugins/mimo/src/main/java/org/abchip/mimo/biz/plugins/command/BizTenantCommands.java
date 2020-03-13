e49c78654097d97b0d463feeae7be4f21cf6ba2d
dev@abchip.it <dev@abchip.it>
2020-03-13 19:00:21 +0100

    Update Seeds

--- org.abchip.mimo.biz.plugins/plugins/mimo/src/main/java/org/abchip/mimo/biz/plugins/command/BizTenantCommands.java
+++ org.abchip.mimo.biz.plugins/plugins/mimo/src/main/java/org/abchip/mimo/biz/plugins/command/BizTenantCommands.java
@@ -36,12 +36,12 @@ public class BizTenantCommands extends BaseCommandProviderImpl {
 		Context context = this.getContext();
 
 		BizCommandUtils.loadSeeds(context, "seed", null, true);
-//		BizCommandUtils.loadSeed(context, "mimo", null, true);
-//		BizCommandUtils.loadSeed(context, "party", null, true);
-//		BizCommandUtils.loadSeed(context, "abchip-net", null, true);
-//		BizCommandUtils.loadSeed(context, "abchip-net-accounting", null, true);
-//		BizCommandUtils.loadSeed(context, "passport", null, true);
-//		BizCommandUtils.loadSeed(context, "edi", null, true);
+		BizCommandUtils.loadSeed(context, "mimo", null, true);
+		BizCommandUtils.loadSeed(context, "party", null, true);
+		BizCommandUtils.loadSeed(context, "abchip-net", null, true);
+		BizCommandUtils.loadSeed(context, "abchip-net-accounting", null, true);
+		BizCommandUtils.loadSeed(context, "passport", null, true);
+		BizCommandUtils.loadSeed(context, "edi", null, true);
 	}
 
 	@SuppressWarnings("resource")
@@ -58,7 +58,6 @@ public class BizTenantCommands extends BaseCommandProviderImpl {
 		String partyId = interpreter.nextArgument();
 		
 		BizCommandUtils.createTenant(context, tenantId, tenantName, false);
-		BizCommandUtils.loadSeeds(context, "seed-initial", null, true);
 		BizCommandUtils.loadSeeds(context, "seed", null, true);
 		BizCommandUtils.loadSeed(context, "mimo", tenantId, true);
 		BizCommandUtils.loadSeed(context, "party", tenantId, true);
@@ -93,7 +92,6 @@ public class BizTenantCommands extends BaseCommandProviderImpl {
 			clean = Boolean.parseBoolean(cleanString);
 
 		BizCommandUtils.createTenant(context, tenantId, tenantName, clean);
-		BizCommandUtils.loadSeeds(context, "seed-initial", null, true);
 		BizCommandUtils.loadSeeds(context, "seed", null, true);
 		BizCommandUtils.loadSeed(context, "mimo", tenantId, true);
 		BizCommandUtils.loadSeed(context, "party", tenantId, true);
