75422c08d952f223758c60c0b8717af8a6d3a431
mattia <dev@abchip.it>
2020-03-13 18:28:23 +0100

    Product updates

--- org.abchip.mimo.biz.plugins/plugins/mimo/src/main/java/org/abchip/mimo/biz/plugins/application/BizApplicationHook.java
+++ org.abchip.mimo.biz.plugins/plugins/mimo/src/main/java/org/abchip/mimo/biz/plugins/application/BizApplicationHook.java
@@ -11,6 +11,11 @@ package org.abchip.mimo.biz.plugins.application;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 
@@ -24,24 +29,39 @@ import org.abchip.mimo.application.ComponentStatus;
 import org.abchip.mimo.biz.BizComponent;
 import org.abchip.mimo.biz.BizModule;
 import org.apache.commons.io.FileUtils;
+import org.apache.ofbiz.base.component.ComponentConfig;
+import org.apache.ofbiz.base.container.Container;
+import org.apache.ofbiz.base.container.ContainerConfig;
+import org.apache.ofbiz.base.container.ContainerException;
+import org.apache.ofbiz.base.start.Config;
 import org.apache.ofbiz.base.start.Start;
+import org.apache.ofbiz.base.start.StartupCommand;
+import org.apache.ofbiz.base.start.StartupException;
 import org.apache.ofbiz.base.util.Debug;
+import org.apache.ofbiz.base.util.UtilValidate;
+
+import edu.emory.mathcs.backport.java.util.Collections;
 
 public class BizApplicationHook {
 
 	@Inject
 	private static Application APPLICATION;
 
-	private static String MODULE = null;
+	protected static String MODULE = null;
+
+	protected static List<Container> CONTAINERS = new LinkedList<>();
 
 	protected static Application getApplication() {
 		return APPLICATION;
 	}
 
+	@SuppressWarnings("resource")
 	@ApplicationStarting
 	private void starting() {
 
 		MODULE = "Application[" + APPLICATION.getName() + "]";
+
+		Debug.logInfo("Starting application: " + APPLICATION.getName(), MODULE);
 
 		ClassLoader parentLoader = Thread.currentThread().getContextClassLoader();
 		ClassLoader bizLoader = new BizClassLoaderImpl(parentLoader);
@@ -59,20 +79,6 @@ public class BizApplicationHook {
 		System.setProperty("derby.system.home", APPLICATION.getPaths().getData() + "/derby");
 
 		Start.main(new String[0]);
-	}
-
-	@ApplicationStarted
-	private void started() {
-	}
-
-	@ApplicationStopping
-	private void stopping() {
-	}
-
-	@ApplicationStopped
-	private void stopped() {
-		ClassLoader bizLoader = Thread.currentThread().getContextClassLoader();
-		Thread.currentThread().setContextClassLoader(bizLoader.getParent());
 	}
 
 	private void copyToWork(Application application, Path workPath) throws IOException {
@@ -109,20 +115,109 @@ public class BizApplicationHook {
 		}
 	}
 
-	private class BizClassLoaderImpl extends ClassLoader {
+	@ApplicationStarted
+	private void started() throws StartupException {
 
-		public BizClassLoaderImpl(ClassLoader originalLoader) {
-			super(originalLoader);
+		Debug.logInfo("Started application: " + APPLICATION.getName(), MODULE);
+
+		Config config = Start.getInstance().getConfig();
+		List<ContainerConfig.Configuration> componentContainerConfigs = filterContainersHavingMatchingLoaders(config.loaders, ComponentConfig.getAllConfigurations());
+		BizApplicationHook.CONTAINERS.addAll(loadContainersFromConfigurations(componentContainerConfigs, config, new ArrayList<StartupCommand>()));
+
+		// Start all containers
+		startLoadedContainers();
+	}
+
+	private List<ContainerConfig.Configuration> filterContainersHavingMatchingLoaders(List<String> loaders, Collection<ContainerConfig.Configuration> containerConfigs) {
+		return containerConfigs.stream().filter(
+				containerCfg -> UtilValidate.isEmpty(containerCfg.loaders) && UtilValidate.isEmpty(loaders) || containerCfg.loaders.stream().anyMatch(loader -> loaders.contains(loader)))
+				.collect(Collectors.toList());
+	}
+
+	private List<Container> loadContainersFromConfigurations(List<ContainerConfig.Configuration> containerConfigs, Config config, List<StartupCommand> ofbizCommands) throws StartupException {
+
+		List<Container> loadContainers = new ArrayList<>();
+		for (ContainerConfig.Configuration containerCfg : containerConfigs) {
+			Debug.logInfo("Loading container: " + containerCfg.name, MODULE);
+			Container tmpContainer = loadContainer(config.containerConfig, containerCfg, ofbizCommands);
+			loadContainers.add(tmpContainer);
+			Debug.logInfo("Loaded container: " + containerCfg.name, MODULE);
+		}
+		return loadContainers;
+	}
+
+	private Container loadContainer(String configFile, ContainerConfig.Configuration containerCfg, List<StartupCommand> ofbizCommands) throws StartupException {
+		// load the container class
+		ClassLoader loader = Thread.currentThread().getContextClassLoader();
+		Class<?> containerClass;
+		try {
+			containerClass = loader.loadClass(containerCfg.className);
+		} catch (ClassNotFoundException e) {
+			throw new StartupException("Cannot locate container class", e);
+		}
+		if (containerClass == null) {
+			throw new StartupException("Component container class not loaded");
 		}
 
-		@Override
-		public Class<?> loadClass(String name) throws ClassNotFoundException {
+		// create a new instance of the container object
+		Container containerObj;
+		try {
+			containerObj = (Container) containerClass.getDeclaredConstructor().newInstance();
+		} catch (ReflectiveOperationException e) {
+			throw new StartupException("Cannot create " + containerCfg.name, e);
+		}
+		if (containerObj == null) {
+			throw new StartupException("Unable to create instance of component container");
+		}
 
-			if (name.equals("org.apache.ofbiz.base.container.ContainerLoader")) {
-				return BizApplicationLoaderImpl.class;
-			} else {
-				return super.loadClass(name);
+		// initialize the container object
+		try {
+			containerObj.init(ofbizCommands, containerCfg.name, configFile);
+		} catch (ContainerException e) {
+			throw new StartupException("Cannot init() " + containerCfg.name, e);
+		}
+
+		return containerObj;
+	}
+
+	private void startLoadedContainers() throws StartupException {
+
+		for (Container container : CONTAINERS) {
+			Debug.logInfo("Starting container " + container.getName(), MODULE);
+			try {
+				container.start();
+			} catch (ContainerException e) {
+				throw new StartupException("Cannot start() " + container.getClass().getName(), e);
 			}
+			Debug.logInfo("Started container " + container.getName(), MODULE);
 		}
+	}
+
+	@ApplicationStopping
+	private void stopping() {
+
+		Debug.logInfo("Stopping application", MODULE);
+
+		List<Container> reversedContainerList = new ArrayList<>(CONTAINERS);
+		Collections.reverse(reversedContainerList);
+
+		for (Container loadedContainer : reversedContainerList) {
+			Debug.logInfo("Stopping container " + loadedContainer.getName(), MODULE);
+			try {
+				loadedContainer.stop();
+			} catch (ContainerException e) {
+				Debug.logError(e, MODULE);
+			}
+			Debug.logInfo("Stopped container " + loadedContainer.getName(), MODULE);
+		}
+
+		Debug.logInfo("Stopped application", MODULE);
+
+	}
+
+	@ApplicationStopped
+	private void stopped() {
+		ClassLoader bizLoader = Thread.currentThread().getContextClassLoader();
+		Thread.currentThread().setContextClassLoader(bizLoader.getParent());
 	}
 }
