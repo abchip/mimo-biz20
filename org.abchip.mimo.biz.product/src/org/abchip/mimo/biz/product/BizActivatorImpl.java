6ed3a1352016d8e9f5d17d3a020dbd6aaf8a11e5
mattia <dev@abchip.it>
2020-03-14 20:24:41 +0100

    Product updates

---
+++ org.abchip.mimo.biz.product/src/org/abchip/mimo/biz/product/BizActivatorImpl.java
@@ -0,0 +1,28 @@
+/**
+ *  Copyright (c) 2017, 2020 ABChip and others.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ */
+package org.abchip.mimo.biz.product;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class BizActivatorImpl implements BundleActivator {
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
