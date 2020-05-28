a5761b393adf6fbe793b238d3045f29a1a0bcc32
mattia <dev@abchip.it>
2020-05-28 17:52:05 +0200

    Authentication refactoring

--- org.abchip.mimo.biz.base/src/org/abchip/mimo/biz/base/GitHubAuthenticationProviderImpl.java
+++ org.abchip.mimo.biz.base/src/org/abchip/mimo/biz/base/GitHubAuthenticationProviderImpl.java
@@ -12,13 +12,12 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.HashMap;
-import java.util.UUID;
 
 import org.abchip.mimo.authentication.AuthenticationException;
 import org.abchip.mimo.authentication.AuthenticationFactory;
 import org.abchip.mimo.authentication.AuthenticationProvider;
 import org.abchip.mimo.authentication.AuthenticationUserToken;
-import org.abchip.mimo.biz.model.passport.OAuth2Google;
+import org.abchip.mimo.biz.model.passport.OAuth2GitHub;
 import org.abchip.mimo.context.Context;
 import org.abchip.mimo.resource.ResourceException;
 import org.abchip.mimo.resource.ResourceReader;
@@ -35,12 +34,12 @@ import org.apache.http.impl.client.HttpClients;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class GoogleAuthenticationProviderImpl implements AuthenticationProvider {
+public class GitHubAuthenticationProviderImpl implements AuthenticationProvider {
 
-	public static final String AUTHORIZE_URI = "https://accounts.google.com/o/oauth2/auth";
-	public static final String DEFAULT_SCOPE = "openid email profile";
+	public static final String AUTHORIZE_URI = "https://github.com/login/oauth/authorize";
+	public static final String DEFAULT_SCOPE = "user,gist,user:email";
 
-	public static final String TokenServiceUri = "https://oauth2.googleapis.com/token";
+	public static final String TokenServiceUri = "https://github.com/login/oauth/access_token";
 
 	public static final RequestConfig StandardRequestConfig = RequestConfig.custom().setCookieSpec(CookieSpecs.STANDARD).build();
 
@@ -48,18 +47,17 @@ public class GoogleAuthenticationProviderImpl implements AuthenticationProvider 
 	public String getRedirectLocation(Context context, String contextId) throws AuthenticationException {
 
 		try {
-			ResourceReader<OAuth2Google> oauth2Reader = context.getResourceManager().getResourceReader(OAuth2Google.class);
-			OAuth2Google oauth2Google = oauth2Reader.first();
+			ResourceReader<OAuth2GitHub> oauth2Reader = context.getResourceManager().getResourceReader(OAuth2GitHub.class);
+			OAuth2GitHub oauth2GitHub = oauth2Reader.first();
 
-			if (oauth2Google == null)
-				throw new AuthenticationException("Google provider not found");
+			if (oauth2GitHub == null)
+				throw new AuthenticationException("GitHub provider not found");
 
 			URIBuilder uri = new URIBuilder(AUTHORIZE_URI);
-			uri.addParameter("client_id", oauth2Google.getClientId());
-			uri.addParameter("redirect_uri", oauth2Google.getReturnUrl());
+			uri.addParameter("client_id", oauth2GitHub.getClientId());
+			uri.addParameter("redirect_uri", oauth2GitHub.getReturnUrl());
 			uri.addParameter("response_type", "code");
 			uri.addParameter("scope", DEFAULT_SCOPE);
-			uri.addParameter("nonce", UUID.randomUUID().toString());
 			uri.addParameter("state", contextId);
 
 			return uri.build().toString();
@@ -72,21 +70,21 @@ public class GoogleAuthenticationProviderImpl implements AuthenticationProvider 
 	public AuthenticationUserToken checkAccessToken(Context context, String authorizationCode) throws AuthenticationException {
 
 		if (Strings.isEmpty(authorizationCode))
-			throw new AuthenticationException("Google failed fo get authorization code");
+			throw new AuthenticationException("GitHub failed fo get authorization code");
 
 		String accessToken = null;
 		String idToken = null;
 
 		try (CloseableHttpClient client = HttpClients.custom().build()) {
 
-			ResourceReader<OAuth2Google> oauth2Reader = context.getResourceManager().getResourceReader(OAuth2Google.class);
-			OAuth2Google oauth2Google = oauth2Reader.first();
+			ResourceReader<OAuth2GitHub> oauth2Reader = context.getResourceManager().getResourceReader(OAuth2GitHub.class);
+			OAuth2GitHub oauth2GitHub = oauth2Reader.first();
 
-			if (oauth2Google == null)
-				throw new AuthenticationException("Google provider not found");
+			if (oauth2GitHub == null)
+				throw new AuthenticationException("GitHub provider not found");
 
-			URI uri = new URIBuilder(TokenServiceUri).setParameter("client_id", oauth2Google.getClientId()).setParameter("client_secret", oauth2Google.getClientSecret())
-					.setParameter("grant_type", "authorization_code").setParameter("code", authorizationCode).setParameter("redirect_uri", oauth2Google.getReturnUrl()).build();
+			URI uri = new URIBuilder(TokenServiceUri).setParameter("client_id", oauth2GitHub.getClientId()).setParameter("client_secret", oauth2GitHub.getClientSecret())
+					.setParameter("grant_type", "authorization_code").setParameter("code", authorizationCode).setParameter("redirect_uri", oauth2GitHub.getReturnUrl()).build();
 
 			HttpPost postMethod = new HttpPost(uri);
 			postMethod.setConfig(StandardRequestConfig);
@@ -99,7 +97,7 @@ public class GoogleAuthenticationProviderImpl implements AuthenticationProvider 
 					idToken = (String) userMap.get("id_token");
 					// Debug.logInfo("Generated Access Token : " + accessToken, module);
 				} else {
-					throw new AuthenticationException("Google get OAuth2 access token error");
+					throw new AuthenticationException("GitHub get OAuth2 access token error");
 				}
 			}
 		} catch (ResourceException | URISyntaxException | IOException e) {
@@ -110,7 +108,7 @@ public class GoogleAuthenticationProviderImpl implements AuthenticationProvider 
 		AuthenticationUserToken authenticationUserToken = AuthenticationFactory.eINSTANCE.createAuthenticationUserToken();
 		authenticationUserToken.setIdToken(idToken);
 		authenticationUserToken.setAccessToken(accessToken);
-		authenticationUserToken.setProvider("Google");
+		authenticationUserToken.setProvider("GitHub");
 
 		return authenticationUserToken;
 	}
