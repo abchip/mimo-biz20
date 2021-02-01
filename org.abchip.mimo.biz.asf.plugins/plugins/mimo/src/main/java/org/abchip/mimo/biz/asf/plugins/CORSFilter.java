7c336b9b6c3bc9add99d6fc87e46c3011fc3e953
mattia <dev@abchip.it>
2021-02-01 20:05:39 +0100

    CORS filter

---
+++ org.abchip.mimo.biz.asf.plugins/plugins/mimo/src/main/java/org/abchip/mimo/biz/asf/plugins/CORSFilter.java
@@ -0,0 +1,46 @@
+/**
+ *  Copyright (c) 2017, 2021 ABChip and others.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ */
+package org.abchip.mimo.biz.asf.plugins;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+public class CORSFilter implements Filter {
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
+		// HttpServletRequest httpRequest = (HttpServletRequest) request;
+
+		httpResponse.setHeader("Access-Control-Allow-Origin", "*");
+		httpResponse.setHeader("Access-Control-Allow-Credentials", "true");
+		httpResponse.setHeader("Access-Control-Allow-Methods", "*");
+		httpResponse.setHeader("Access-Control-Max-Age", "3600");
+		httpResponse.setHeader("Access-Control-Allow-Headers", "*");
+		httpResponse.setHeader("Access-Control-Expose-Headers", "Location" + ", " + "Origin");
+
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void destroy() {
+	}
+}
--- org.abchip.mimo.biz.base/src/org/abchip/mimo/biz/base/BizAuthenticationManagerImpl.java
+++ org.abchip.mimo.biz.base/src/org/abchip/mimo/biz/base/BizAuthenticationManagerImpl.java
@@ -17,39 +17,129 @@ import org.abchip.mimo.application.Application;
 import org.abchip.mimo.authentication.AuthenticationAdminKey;
 import org.abchip.mimo.authentication.AuthenticationAnonymous;
 import org.abchip.mimo.authentication.AuthenticationException;
+import org.abchip.mimo.authentication.AuthenticationFactory;
 import org.abchip.mimo.authentication.AuthenticationManager;
 import org.abchip.mimo.authentication.AuthenticationUserPassword;
 import org.abchip.mimo.authentication.AuthenticationUserToken;
 import org.abchip.mimo.biz.model.entity.tenant.Tenant;
 import org.abchip.mimo.biz.model.security.login.UserLogin;
+import org.abchip.mimo.biz.service.security.CheckExternalLoginUser;
+import org.abchip.mimo.biz.service.security.CheckExternalLoginUserResponse;
+import org.abchip.mimo.biz.service.security.UserCredentialFromExternalId;
+import org.abchip.mimo.biz.service.security.UserCredentialFromExternalIdResponse;
 import org.abchip.mimo.context.Context;
 import org.abchip.mimo.context.ContextHandler;
+import org.abchip.mimo.context.ProviderConfig;
+import org.abchip.mimo.context.ProviderUser;
+import org.abchip.mimo.core.http.HttpUtils;
 import org.abchip.mimo.entity.Frame;
 import org.abchip.mimo.resource.ResourceException;
 import org.abchip.mimo.resource.ResourceReader;
+import org.abchip.mimo.service.ServiceManager;
+import org.abchip.mimo.util.Logs;
+import org.osgi.service.log.Logger;
 
 public class BizAuthenticationManagerImpl implements AuthenticationManager {
 
 	@Inject
 	private Application application;
+	@Inject
+	private ProviderConfig providerConfig;
 
 	private Map<String, Context> contextTenants = new WeakHashMap<String, Context>();
 
+	private static final Logger LOGGER = Logs.getLogger(BizAuthenticationManagerImpl.class);
+
 	@Override
 	public boolean checkLogin(AuthenticationUserToken authentication, boolean create) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			ServiceManager serviceManager = application.getContext().getServiceManager();
+			CheckExternalLoginUser checkExternalLoginUser = application.getContext().getServiceManager().prepare(CheckExternalLoginUser.class);
+			checkExternalLoginUser.setProvider(authentication.getProvider());
+			checkExternalLoginUser.setAccessToken(authentication.getAccessToken());
+
+			Map<String, Object> userInfo = null;
+			switch (authentication.getProvider()) {
+			case "GitHub": {
+				userInfo = HttpUtils.checkLoginGitHub(authentication);
+				authentication.setUser((String) userInfo.get("email"));
+				authentication.setPicture((String) userInfo.get("avatar_url"));
+
+				checkExternalLoginUser.setUserId((String) userInfo.get("email"));
+				checkExternalLoginUser.setEmail((String) userInfo.get("email"));
+				checkExternalLoginUser.setFirstName((String) userInfo.get("name"));
+				checkExternalLoginUser.setLastName((String) userInfo.get("lastName"));
+				break;
+			}
+			case "Google": {
+				userInfo = HttpUtils.checkLoginGoogle(authentication);
+				authentication.setUser((String) userInfo.get("email"));
+				authentication.setPicture((String) userInfo.get("picture"));
+
+				checkExternalLoginUser.setUserId((String) userInfo.get("email"));
+				checkExternalLoginUser.setEmail((String) userInfo.get("email"));
+				checkExternalLoginUser.setFirstName((String) userInfo.get("given_name"));
+				checkExternalLoginUser.setLastName((String) userInfo.get("family_name"));
+				break;
+			}
+			case "LinkedIn": {
+				userInfo = HttpUtils.checkLoginLinkedIn(authentication);
+				authentication.setUser((String) userInfo.get("email"));
+				authentication.setPicture((String) userInfo.get("picture"));
+
+				checkExternalLoginUser.setUserId((String) userInfo.get("email"));
+				checkExternalLoginUser.setEmail((String) userInfo.get("email"));
+				checkExternalLoginUser.setFirstName((String) userInfo.get("localizedFirstName"));
+				checkExternalLoginUser.setLastName((String) userInfo.get("localizedLastName"));
+				break;
+			}
+			default:
+				LOGGER.audit("Unknown provider {} for user {}", authentication.getProvider(), authentication.getUser());
+				return false;
+			}
+
+			CheckExternalLoginUserResponse response = serviceManager.execute(checkExternalLoginUser);
+			if (response.onError())
+				LOGGER.error(response.getErrorMessage());
+
+			LOGGER.audit("CheckLogin success id {} user {} provider {}", authentication.getIdToken(), authentication.getUser(), authentication.getProvider());
+		} catch (Exception e) {
+			LOGGER.audit("Invalid check login provider {} for user {} message {}", authentication.getProvider(), authentication.getUser(), e.getMessage());
+			return false;
+		}
+
+		return true;
 	}
 
 	@Override
 	public ContextHandler login(String contextId, AuthenticationAnonymous authentication) throws AuthenticationException {
-		return null;
+
+		ProviderUser user = this.providerConfig.getPublicUser();
+		if (user == null)
+			return null;
+
+		AuthenticationUserPassword authenticationUserPassword = AuthenticationFactory.eINSTANCE.createAuthenticationUserPassword();
+		authenticationUserPassword.setTenant(authentication.getTenant());
+		authenticationUserPassword.setUser(user.getUser());
+		authenticationUserPassword.setPassword(user.getPassword());
+
+		ContextHandler context = this.login(contextId, authenticationUserPassword);
+		if (context != null)
+			context.getContext().getContextDescription().setAnonymous(true);
+
+		return context;
 	}
 
 	@Override
 	public ContextHandler login(String contextId, AuthenticationUserToken authentication) throws AuthenticationException {
-		// TODO Auto-generated method stub
-		return null;
+
+		AuthenticationUserPassword authenticationUserPassword = getExternalCredentials(authentication.getUser());
+
+		ContextHandler context = this.login(contextId, authenticationUserPassword);
+
+		context.getContext().getContextDescription().setPicture(authentication.getPicture());
+
+		return context;
 	}
 
 	@Override
@@ -82,7 +172,7 @@ public class BizAuthenticationManagerImpl implements AuthenticationManager {
 		contextUser.getContextDescription().setTimeZone(userLogin.getLastTimeZone());
 
 		contextUser.set(UserLogin.class, userLogin);
-		
+
 		return contextHandler;
 	}
 
@@ -136,5 +226,30 @@ public class BizAuthenticationManagerImpl implements AuthenticationManager {
 		}
 
 		return contextTenant;
+	}
+
+	private AuthenticationUserPassword getExternalCredentials(String user) throws AuthenticationException {
+
+		LOGGER.warn("Unsecure access to external credential for user {}", user);
+
+		AuthenticationUserPassword authentication = AuthenticationFactory.eINSTANCE.createAuthenticationUserPassword();
+		try {
+			ServiceManager serviceManager = application.getContext().getServiceManager();
+			UserCredentialFromExternalId userCredentialFromExternalId = application.getContext().getServiceManager().prepare(UserCredentialFromExternalId.class);
+			userCredentialFromExternalId.setUserId(user);
+			UserCredentialFromExternalIdResponse response = serviceManager.execute(userCredentialFromExternalId);
+			if (response.onError())
+				LOGGER.error(response.getErrorMessage());
+
+			authentication.setUser(response.getUser());
+			authentication.setPassword(response.getPassword());
+
+			LOGGER.audit("External credential success user {}", user);
+		} catch (Exception e) {
+			LOGGER.audit("Invalid get external credentials for user {} message", user, e.getMessage());
+			throw new AuthenticationException(e);
+		}
+
+		return authentication;
 	}
 }
